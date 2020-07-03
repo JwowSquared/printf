ff57fdd7cebca5d9dd889fe21ef1fc55511004fd
JwowSquared <JwowRS@hotmail.com>
2020-07-03 18:21:18 +0000

    added functionality to print integers

---
+++ conversion_di.c
@@ -0,0 +1,20 @@
+#include "printf.h"
+#include <stdarg.h>
+
+int printInteger(va_list input)
+{
+	int out = va_arg(input, int);
+	return (recursion(out));
+}
+
+int int_recursion(int i)
+{
+	int digit, total;
+
+	if (i == 0)
+		return (0);
+	digit = i % 10;
+	total = 1 + recursion(i / 10);
+	_putchar(digit + 48);
+	return (total);
+}
