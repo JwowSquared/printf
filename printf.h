ccdd008856ed82495133bfe17fe4cafa8646081c
JwowSquared <JwowRS@hotmail.com>
2020-07-03 18:20:59 +0000

    fixed struct definition and added prototypes for printChar and printString

--- printf.h
+++ printf.h
@@ -3,8 +3,16 @@
 
 #include <stdarg.h>
 
+typedef struct specifier
+{
+	char *key;
+	int (*f)(va_list);
+} spec;
+
 int _putchar(char c);
 int printInteger(va_list input);
-int recursion(int i);
+int int_recursion(int i);
+int printChar(va_list input);
+int printString(va_list input);
 
 #endif /* _PRINTF_ */
