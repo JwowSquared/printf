c6afc21c454967138d125ca67f5b48a471e54693
JwowSquared <JwowRS@hotmail.com>
2020-07-04 00:17:47 +0000

    changed file name to holberton

---
+++ holberton.h
@@ -0,0 +1,23 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct specifier
+{
+	char *key;
+	int (*f)(va_list);
+} spec;
+
+int _printf(const char *str, ...);
+int _putchar(char c);
+int printInteger(va_list input);
+int int_recursion(int i);
+int printChar(va_list input);
+int printString(va_list input);
+int printBinary(va_list input);
+int binary_recursion(unsigned int i);
+
+#endif /* _PRINTF_ */
