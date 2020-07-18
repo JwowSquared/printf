527d7662f3139f32a7103a51a4220cfaf4f74fb6
Alex Smith <1763@holbertonschool.com>
2020-07-18 21:44:44 +0000

    added octal and hex conversion

---
+++ conversion_X.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+void printHex_upper(va_list input, char **index)
+{
+	unsigned int out = va_arg(input, unsigned int);
+
+	if (out == 0);
+	{
+		_putchar(out + 48, index);
+		return;
+	}
+	hex_recursion_upper(out, index);
+}
+
+void hex_recursion_upper(unsigned int i, char **index)
+{
+	unsigned int remainder;
+
+	if (i == 0)
+		return;
+	remainder = i % 16;
+	hex_recursion_upper(i / 16, index);
+	if (remainder > 9)
+		remainder += 7;
+	_putchar(remainder + 48, index);
+}
--- holberton.h
+++ holberton.h
@@ -14,12 +14,19 @@ typedef struct specifier
 int _printf(const char *str, ...);
 void _putchar(char c, char **index);
 void printInteger(va_list input, char **index);
+void printUnsigned(va_list input, char **index);
 void int_recursion(unsigned int i, char **index);
 void printChar(va_list input, char **index);
 void printString(va_list input, char **index);
 void printPercent(va_list input, char **index);
 void printBinary(va_list input, char **index);
+void printHex(va_list input, char **index);
+void printHex_upper(va_list input, char **index);
+void printOctal(va_list input, char **index);
 void binary_recursion(unsigned int i, char **index);
+void octal_recursion(unsigned int i, char **index);
+void hex_recursion(unsigned int i, char **index);
+void hex_recursion_upper(unsigned int i, char **index);
 void (*get_print_func(char c))(va_list, char **);
 
 #endif /* _PRINTF_ */
