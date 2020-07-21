a145c6c3c9a81389330743f762a2da18198b1b80
Alex Smith <1763@holbertonschool.com>
2020-07-21 07:03:15 +0000

    Fixed betty issues

--- conversion_uox.c
+++ conversion_uox.c
@@ -4,6 +4,7 @@
  * printUnsigned - Prints an unsigned int
  * @input: arugments
  * @index: buffer
+ * @m: modifiers
  * Return: no return
  */
 void printUnsigned(va_list input, mods *m, char **index)
@@ -18,6 +19,7 @@ void printUnsigned(va_list input, mods *m, char **index)
 /**
  * printOctal - prints a number in octal
  * @input: arugments
+ * @m: modifiers
  * @index: buffer
  * Return: no return
  */
@@ -58,6 +60,7 @@ void octal_recursion(unsigned int i, char **index)
  * printHex - prints a number in hex
  * @input: arugments
  * @index: buffer
+ * @m: modifiers
  * Return: no return
  */
 void printHex(va_list input, mods *m, char **index)
