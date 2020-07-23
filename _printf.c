b1e4ad7f75f038c9f5b7c1d4ae69ad89cb34f942
JwowSquared <JwowRS@hotmail.com>
2020-07-23 13:48:43 +0000

    properly return -1 if % is the last character of a string, but allow other wrong specs to be printed

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, total = 0, *j = &i;
+	int i = 0, k = 0, c = 0, total = 0, *j = &i;
 	int (*f)(va_list, mods *, char **);
 	va_list input;
 	char buffer[1024], *p = buffer, **index = &p;
@@ -17,17 +17,19 @@ int _printf(const char *format, ...)
 
 	if (m == NULL || format == NULL)
 		return (-1);
-
-	for (i = 0; i < 1023; i++)
-		buffer[i] = '!';
+	for (k = 0; k < 1023; k++)
+		buffer[k] = '!';
 	buffer[1023] = '\0';
 	va_start(input, format);
-	i = 0;
 	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%')
 		{
-			i++;
+			if (format[++i] == '\0')
+			{
+				free(m);
+				return (-1);
+			}
 			get_mods(format, m, j);
 			f = get_print_func(m->key);
 			if (f != NULL)
@@ -36,17 +38,15 @@ int _printf(const char *format, ...)
 				i++;
 				continue;
 			}
-			else
-				i--;
+			i--;
 		}
 		total += _putchar(format[i++], index);
 	}
 	va_end(input);
 	free(m);
-	i = 0;
 	**index = '\0';
-	while (buffer[i])
-		i++;
-	write(1, &buffer, i);
-	return (total + i);
+	while (buffer[c])
+		c++;
+	write(1, &buffer, c);
+	return (total + c);
 }
